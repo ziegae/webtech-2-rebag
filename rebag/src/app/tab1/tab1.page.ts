ae32ff1c296ef7dfbe16f5d763da172d1798020c
ziegae <zoe.then-bergh@hs-osnabrueck.de>
2021-02-27 11:37:49 +0100

    add google maps on first slide

--- rebag/src/app/tab1/tab1.page.ts
+++ rebag/src/app/tab1/tab1.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit, AfterViewInit {
+  map: google.maps.Map;
 
-  constructor() {}
+  @ViewChild('mapElement') mapElement:ElementRef;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+   ngAfterViewInit() {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      center: {lat: 52, lng: 8},
+      zoom: 8
+    });
+
+    if (navigator.geolocation)
+    {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const latLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+        this.map.setCenter(latLng);
+        this.map.setZoom(14);
+      });
+    }
+   }
 
 }
