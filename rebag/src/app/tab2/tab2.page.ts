9389ad2878114271a85ef828a256a99a79e64bcf
Jan <jan.brinkmann@hs-osnabrueck.de>
2021-03-06 16:03:53 +0100

    standort hinzufügen

--- rebag/src/app/tab2/tab2.page.ts
+++ rebag/src/app/tab2/tab2.page.ts
@@ -1,14 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { MarkersService } from '../services/pins.service';
+import { Plugins, CameraResultType } from '@capacitor/core';
+const { Camera } = Plugins;
+const { Geolocation } = Plugins;
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  pinId:string ='';
+  name:string = '';
+  bagsAvailable:boolean;
+  bagsClean:boolean;
+  latitude:number = 0;
+  longitude:number = 0;
+  availabilityReport:number = 0;
+  cleaningReport:number = 0;
+  imageBase64:any = null;
 
-  
+  constructor(private markersService:MarkersService, public modalController:ModalController) {}
+
+  ngOnInit() {}
+
+  ionViewDidEnter(){
+    this.latitude = 0.0;
+    this.longitude = 0.0;
+
+    const coordinates = Geolocation.getCurrentPosition().then((pos) => {
+      this.latitude = pos.coords.latitude;
+      this.longitude = pos.coords.longitude;
+    });
+  }
+
+  async takePhoto(){
+    const image = await Camera.getPhoto({
+      quality: 20,
+      allowEditing: true,
+      resultType: CameraResultType.Base64
+    });
+    this.imageBase64 = 'data:image/'+image.format+';base64,'+image.base64String;
+  }
+
+  save(){
+    const marker = {
+      'pinId': this.pinId,
+      'name': this.name,
+      'basAvailable':this.bagsAvailable,
+      'bagsClean': this.bagsClean,
+      'coordinates': [this.latitude, this.longitude],
+      'availabilityReport': this.availabilityReport,
+      'cleaningReport': this.cleaningReport,
+      'imageBase64': this.imageBase64
+    };
+    this.markersService.add(marker);
+    this.pinId = '';
+    this.name = '';
+    this.bagsAvailable = false;
+    this.bagsClean = false;
+    this.latitude = 0;
+    this.longitude = 0;
+    this.availabilityReport = 0;
+    this.cleaningReport = 0
+    this.imageBase64 = '';
+  }
 
 }
