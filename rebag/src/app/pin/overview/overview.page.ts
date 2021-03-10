8027618fa50b464b66b0718adaa44b2f54d18dd0
Jan <jan.brinkmann@hs-osnabrueck.de>
2021-03-10 16:16:22 +0100

    Code formatiert und kommentiert

--- rebag/src/app/pin/overview/overview.page.ts
+++ rebag/src/app/pin/overview/overview.page.ts
@@ -1,18 +1,18 @@
 import { ResetCleanPage } from './../reset-clean/reset-clean.page';
 import { ResetAvailabilityPage } from './../reset-availability/reset-availability.page';
-
 import { Component, OnInit } from '@angular/core';
 import { MarkersService } from '../../services/pins.service';
-
 import { ActivatedRoute } from '@angular/router'
-
 import { ModalController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.page.html',
   styleUrls: ['./overview.page.scss'],
 })
+
+
 export class OverviewPage implements OnInit {
   markers: any = [];
   loadedPin: any;
@@ -30,6 +30,7 @@ export class OverviewPage implements OnInit {
   test: any = false;
 
 
+  //Marker laden
   constructor(
     private markersService: MarkersService,
     private activatedRoute: ActivatedRoute,
@@ -37,9 +38,10 @@ export class OverviewPage implements OnInit {
     this.markersService.getMarkersSubject().subscribe(() => {
       this.markers = this.markersService.getMarkers();
     });
-
   }
 
+
+  //Marker und Status abfrage
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
       if (!paramMap.has('pinId')) {
@@ -57,6 +59,8 @@ export class OverviewPage implements OnInit {
     this.reloadStatus();
   }
 
+
+  //Verfügbarkeits Stauts zurücksetzen
   async resetAvailabilityModal() {
     if (!this.loadedPin.bagsAvailable) {
       let modal = await this.modalController.create({ component: ResetAvailabilityPage });
@@ -68,6 +72,8 @@ export class OverviewPage implements OnInit {
     return;
   }
 
+
+  //Sauberkeits Status zurücksetzen
   async resetCleanModal() {
     if (!this.loadedPin.bagsClean) {
       const modal = await this.modalController.create({ component: ResetCleanPage });
@@ -79,11 +85,14 @@ export class OverviewPage implements OnInit {
     return;
   }
 
+
+  //Marker laden
   setOverviewPinId() {
     this.markersService.setOverviewPinId(this.loadedPin);
   }
 
 
+  //Aktuellen Status laden
   reloadStatus() {
     if (this.loadedPin.bagsAvailable) {
       this.imgSrcBagsAvailable = "bagsAvailable_status-true.svg";
