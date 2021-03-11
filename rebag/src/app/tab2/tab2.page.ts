2491ed6fa6a93030b5740a4cd4415090b0c9a90b
ziegae <zoe.then-bergh@hs-osnabrueck.de>
2021-03-11 22:47:00 +0100

    link zu ideen hinzugefügt

--- rebag/src/app/tab2/tab2.page.ts
+++ rebag/src/app/tab2/tab2.page.ts
@@ -23,6 +23,7 @@ export class Tab2Page implements OnInit {
   imageBase64: any = null;
   toggle: boolean = true;
   isShown = false;
+  imageTook= false;
 
   constructor(private markersService: MarkersService, public modalController: ModalController) {
 
@@ -48,7 +49,9 @@ export class Tab2Page implements OnInit {
       allowEditing: true,
       resultType: CameraResultType.Base64
     });
-    this.imageBase64 = 'data:image/' + image.format + ';base64,' + image.base64String;
+    this.imageBase64 = image.base64String;
+    this.imageTook=true;
+    console.log("bild aufgenommen")
   }
 
   //Standort speichern
